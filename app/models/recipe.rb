996c273f254b232bc54edbb3931ce07edf991c8a
Daw Jose <joan_garcia88@msn.com>
2016-04-01 07:06:26 +0100

    App para recetas de cocina, modelos de recetas con ingredientes y pasos, modelo de usuarios, modelos relacionados, bootstrap y jquerymobile para frontend, gemas usadas 'devise' para modelo de usuarios 'cocoon' para relacionar los modelos en los formularios 'haml' para simplicaficar html 'simply form' para simplificar formularios 'mini-magick' y 'carrierwave' para la gestion y subida de imagenes, falta que retoques un poco la estetica por ejemplo que se vean en grid las recetas en el index y si acaso poner un buscador de recetas que eso con un scope se hace facil

---
+++ app/models/recipe.rb
@@ -0,0 +1,13 @@
+class Recipe < ActiveRecord::Base
+	belongs_to :user
+
+	has_many :ingredients
+	has_many :directions
+
+	accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true	
+	accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
+
+	validates :title, :description, :image, presence: true
+
+	mount_uploader :image, ImageUploader
+end
---
+++ app/views/recipes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipe, :id, :title, :description, :user_id, :image, :created_at, :updated_at
