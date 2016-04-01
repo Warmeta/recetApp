b350141bc40f9325c4cc6e50a93c38fe95f6f3da
Daw Jose <joan_garcia88@msn.com>
2016-04-01 18:47:34 +0100

    Cache y show quantity

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -70,6 +70,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:title, :description, :user_id, :image, ingredients_attributes: [:id, :name, :quantity, :destroy], directions_attributes: [:id, :step, :destroy])
+      params.require(:recipe).permit(:title, :description, :user_id, :image, ingredients_attributes: [:id, :name, :quatity, :destroy], directions_attributes: [:id, :step, :destroy])
     end
 end
